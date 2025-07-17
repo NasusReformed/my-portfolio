1def59a127f99ece358ff359f2a2601be0c1c508
Nasus Reformed <nasus.informatic.engineering@gmail.com>
2025-07-17 01:44:39 -0400

    feature: add font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { 
+  Inter, 
+  Geist
+} from "next/font/google";
 import SpeedDial from "@/components/common/SpeedDial";
 import Footer from "@/components/common/Footer";
 
 const inter = Inter({
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
+const geist = Geist({
+  subsets: ['latin'],
+})
+
 export const metadata: Metadata = {
-  title: "Portfolio of Informatic Engineer",
-  description: "Profesional portfolio of Ignacio Quevedo, Informatic Engineer",
+  title: "Portfolio of Software Engineer",
+  description: "Profesional portfolio of Ignacio Quevedo, Software Engineer",
 };
 
 export default function RootLayout({
@@ -19,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={geist.className}>
       <body className={inter.className}>
         <main>{children}</main>
         <SpeedDial />{/* Render the Speed Dial here */}
