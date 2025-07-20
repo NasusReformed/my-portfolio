c7b069ff470c19d88d83c72891d6c0f273815e35
Nasus Reformed <nasus.informatic.engineering@gmail.com>
2025-07-20 16:43:02 -0400

    feature: Added new package Prettier ESlint and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,36 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals",
+    "next/typescript",
+  ),
+  {
+    ignores: [
+      "node_modules",
+      ".next",
+      "dist",
+      "build",
+      "coverage",
+      "public",
+      "*.lock",
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: "@typescript-eslint/parser",
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      prettier: require("eslint-plugin-prettier"),
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/components/common/AboutSection.tsx
+++ src/components/common/AboutSection.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const AboutSection: React.FC = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">About Me</h2>
       {/* Aquí irá la presentación personal, foto, etc. */}
-      Hello everyone, My name is Ignacio Quevedo but your can call me Nacho or Nasus.I am a Software Engineer with a passion for technology and continuous learning. I specialize in Full Stack Development, creating innovative solutions that enhance user experiences and drive business success. Recently I have been focused on improving my skills in React, TypeScript, and Node.js, and I am always eager to learn new technologies and methodologies that can help me grow as a developer. I'm learning new technologies as machine learning and AI to stay at the forefront of the industry. This complemented with certifications in Data Analysis and Cloud Computing, allows me to create robust and scalable applications. I believe in the power of collaboration and am always looking for opportunities to work on exciting projects that challenge my skills and expand my knowledge. 
+      <p className="mb-4">
+        Hello everyone, My name is Ignacio Quevedo but your can call me Nacho or
+        Nasus.I am a Software Engineer with a passion for technology and
+        continuous learning. I specialize in Full Stack Development, creating
+        innovative solutions that enhance user experiences and drive business
+        success. Recently I have been focused on improving my skills in React,
+        TypeScript, and Node.js, and I am always eager to learn new technologies
+        and methodologies that can help me grow as a developer. I'm learning new
+        technologies as machine learning and AI to stay at the forefront of the
+        industry. This complemented with certifications in Data Analysis and
+        Cloud Computing, allows me to create robust and scalable applications. I
+        believe in the power of collaboration and am always looking for
+        opportunities to work on exciting projects that challenge my skills and
+        expand my knowledge.
+      </p>
       {/* Aquí pue</p>des agregar más detalles sobre ti, tus intereses, etc. */}
     </div>
   );
