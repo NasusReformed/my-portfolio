e6d4cdd527c791ae78dffdfb56723a833b28c82c
Nasus Reformed <nasus.informatic.engineering@gmail.com>
2025-07-21 23:40:47 -0400

    style: changed the style of About Me

--- src/components/common/AboutSection.tsx
+++ src/components/common/AboutSection.tsx
@@ -1,26 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const AboutSection: React.FC = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">About Me</h2>
-      {/* Aquí irá la presentación personal, foto, etc. */}
-      <p className="mb-4">
-        Hello everyone, My name is Ignacio Quevedo but your can call me Nacho or
-        Nasus.I am a Software Engineer with a passion for technology and
-        continuous learning. I specialize in Full Stack Development, creating
-        innovative solutions that enhance user experiences and drive business
-        success. Recently I have been focused on improving my skills in React,
-        TypeScript, and Node.js, and I am always eager to learn new technologies
-        and methodologies that can help me grow as a developer. I'm learning new
-        technologies as machine learning and AI to stay at the forefront of the
-        industry. This complemented with certifications in Data Analysis and
-        Cloud Computing, allows me to create robust and scalable applications. I
-        believe in the power of collaboration and am always looking for
-        opportunities to work on exciting projects that challenge my skills and
-        expand my knowledge.
+    <div className="w-full max-w-2xl mx-auto flex flex-col items-center justify-center py-12 px-4">
+      <h2 className="text-2xl text-center font-bold mb-4">About Me</h2>
+      <p className="mb-4 text-center text-lg text-gray-700 dark:text-gray-300 leading-relaxed">
+        Hello everyone, My name is Ignacio Quevedo but you can call me Nacho or Nasus. I am a
+        Software Engineer with a passion for technology and continuous learning. I specialize in
+        Software Engineer, creating innovative solutions that enhance user experiences and drive
+        business success. Recently I have been focused on improving my skills in React, TypeScript,
+        Node.js, and Next.js. I am always eager to learn new technologies and methodologies that can
+        help me grow as a developer. I'm learning new technologies such as machine learning and AI
+        to stay at the forefront of the industry. This, complemented with certifications in Data
+        Analysis and Cloud Computing, allows me to create robust and scalable applications. I
+        believe in the power of collaboration and am always looking for opportunities to work on
+        exciting projects that challenge my skills and expand my knowledge.
       </p>
-      {/* Aquí pue</p>des agregar más detalles sobre ti, tus intereses, etc. */}
     </div>
   );
 };
