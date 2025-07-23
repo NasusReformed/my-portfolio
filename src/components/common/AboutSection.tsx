62e9d50e7da018483720fd89e5a319a47635a262
Nasus Reformed <nasus.informatic.engineering@gmail.com>
2025-07-22 23:05:41 -0400

    style: change the text in about me

--- src/components/common/AboutSection.tsx
+++ src/components/common/AboutSection.tsx
@@ -8,13 +8,14 @@ const AboutSection: React.FC = () => {
         Hello everyone, My name is Ignacio Quevedo but you can call me Nacho or Nasus. I am a
         Software Engineer with a passion for technology and continuous learning. I specialize in
         Software Engineer, creating innovative solutions that enhance user experiences and drive
-        business success. Recently I have been focused on improving my skills in React, TypeScript,
-        Node.js, and Next.js. I am always eager to learn new technologies and methodologies that can
-        help me grow as a developer. I'm learning new technologies such as machine learning and AI
-        to stay at the forefront of the industry. This, complemented with certifications in Data
-        Analysis and Cloud Computing, allows me to create robust and scalable applications. I
-        believe in the power of collaboration and am always looking for opportunities to work on
-        exciting projects that challenge my skills and expand my knowledge.
+        business success. Recently I have been focused on improving my skills in Python, Data
+        Analyst with Python, and Computer Science with Python. I am always eager to learn new
+        technologies and methodologies that can help me grow as a developer. I'm learning new
+        technologies such as machine learning and AI to stay at the forefront of the industry. This,
+        complemented with certifications in Data Analysis and Cloud Computing, allows me to create
+        robust and scalable applications. I believe in the power of collaboration and am always
+        looking for opportunities to work on exciting projects that challenge my skills and expand
+        my knowledge.
       </p>
     </div>
   );
